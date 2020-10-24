c2fe296ee75a762a028f81ece8781738a28a22e5
Omar Sanchez Avelar <omar_sanchez_avelar@hotmail.com>
2020-10-24 16:24:45 -0500

    se agregan niveles de dificultad

---
+++ js/niveles.js
@@ -0,0 +1,20 @@
+const FACIL = document.getElementById('facil')
+const MEDIO = document.getElementById('medio')
+const SUPREMO = document.getElementById('supremo')
+let n; 
+
+FACIL.addEventListener("click", () =>
+{
+   return n = "facil"
+})
+
+MEDIO.addEventListener("click", () =>
+{
+   return n = "medio"
+})
+
+SUPREMO.addEventListener("click", () =>
+{
+   return n = "supremo"
+})
+
