20318a03bd454b7aa49d37a435389ae389febd97
Omar Sanchez Avelar <omar_sanchez_avelar@hotmail.com>
2020-10-28 16:20:03 -0600

    se agrega sonido para elegir nivel

--- js/niveles.js
+++ js/niveles.js
@@ -3,6 +3,44 @@ const MEDIO = document.getElementById('medio')
 const SUPREMO = document.getElementById('supremo')
 let n; 
 
+// function level()
+// {
+   
+//    let sonido = document.createElement("audio")
+//    sonido.setAttribute("src", "https://omarsav.com/audio/level.mp3")
+//    sonido.setAttribute("autoplay", "autoplay")
+//    document.body.appendChild(sonido);
+
+//    return n = "facil"
+// }
+
+// // level()
+// return level
+
+function soundLevel()
+{
+   var sonido = document.createElement("audio")
+   sonido.setAttribute("src", "https://omarsav.com/audio/niveles.mp3")
+   sonido.setAttribute("autoplay", "autoplay")
+   document.body.appendChild(sonido);
+   console.log("hola")
+}
+
+FACIL.addEventListener("mouseover", () =>
+{
+   soundLevel()
+})
+
+MEDIO.addEventListener("mouseover", () =>
+{
+   soundLevel()
+})
+
+SUPREMO.addEventListener("mouseover", () =>
+{
+   soundLevel()
+})
+
 FACIL.addEventListener("click", () =>
 {
    return n = "facil"
