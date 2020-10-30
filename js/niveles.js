5b602c198590b4c400f242f2097efeb7c09cd100
Omar Sanchez Avelar <omar_sanchez_avelar@hotmail.com>
2020-10-29 18:54:17 -0600

    se prueba audio de fondo

--- js/niveles.js
+++ js/niveles.js
@@ -1,58 +1,47 @@
-const FACIL = document.getElementById('facil')
-const MEDIO = document.getElementById('medio')
-const SUPREMO = document.getElementById('supremo')
-let n; 
-
-// function level()
+// window.addEventListener('DOMContentLoad', function() 
 // {
-   
-//    let sonido = document.createElement("audio")
-//    sonido.setAttribute("src", "https://omarsav.com/audio/level.mp3")
-//    sonido.setAttribute("autoplay", "autoplay")
-//    document.body.appendChild(sonido);
 
-//    return n = "facil"
-// }
+   const FACIL = document.getElementById('facil')
+   const MEDIO = document.getElementById('medio')
+   const SUPREMO = document.getElementById('supremo')
+   let n; 
 
-// // level()
-// return level
+   function soundLevel()
+   {
+      let sound_level = document.createElement("audio")
+      sound_level.setAttribute("src", "https://omarsav.com/audio/niveles.mp3")
+      sound_level.setAttribute("autoplay", "autoplay")
+      document.body.appendChild(sound_level);
+      console.log("hola")
+   }
 
-function soundLevel()
-{
-   var sonido = document.createElement("audio")
-   sonido.setAttribute("src", "https://omarsav.com/audio/niveles.mp3")
-   sonido.setAttribute("autoplay", "autoplay")
-   document.body.appendChild(sonido);
-   console.log("hola")
-}
+   FACIL.addEventListener("mouseover", () =>
+   {
+      soundLevel()
+   })
 
-FACIL.addEventListener("mouseover", () =>
-{
-   soundLevel()
-})
+   MEDIO.addEventListener("mouseover", () =>
+   {
+      soundLevel()
+   })
 
-MEDIO.addEventListener("mouseover", () =>
-{
-   soundLevel()
-})
+   SUPREMO.addEventListener("mouseover", () =>
+   {
+      soundLevel()
+   })
 
-SUPREMO.addEventListener("mouseover", () =>
-{
-   soundLevel()
-})
+   FACIL.addEventListener("click", () =>
+   {
+      return n = "facil"
+   })
 
-FACIL.addEventListener("click", () =>
-{
-   return n = "facil"
-})
+   MEDIO.addEventListener("click", () =>
+   {
+      return n = "medio"
+   })
 
-MEDIO.addEventListener("click", () =>
-{
-   return n = "medio"
-})
-
-SUPREMO.addEventListener("click", () =>
-{
-   return n = "supremo"
-})
-
+   SUPREMO.addEventListener("click", () =>
+   {
+      return n = "supremo"
+   })
+// })
