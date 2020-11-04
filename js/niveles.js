1dad40eddd1ee763b572058fd1628cb08b824df4
Omar Sanchez Avelar <omar_sanchez_avelar@hotmail.com>
2020-11-03 23:16:46 -0600

    se agregan sonidos al juego-3

--- js/niveles.js
+++ js/niveles.js
@@ -1,46 +1,43 @@
-// window.addEventListener('DOMContentLoad', function() 
-// {
-   const FACIL = document.getElementById('facil')
-   const MEDIO = document.getElementById('medio')
-   const SUPREMO = document.getElementById('supremo')
-   let n
+const FACIL = document.getElementById('facil')
+const MEDIO = document.getElementById('medio')
+const SUPREMO = document.getElementById('supremo')
+let n
 
-   function soundLevel()
-   {
-      let sound_level = document.createElement("audio")
-      sound_level.setAttribute("src", "https://omarsav.com/audio/niveles.mp3")
-      sound_level.setAttribute("autoplay", "autoplay")
-      document.body.appendChild(sound_level)
-      console.log(sound_level)
-   }
+function soundLevel()
+{
+   let sound_level = document.createElement("audio")
+   sound_level.setAttribute("src", "https://omarsav.com/audio/niveles.mp3")
+   sound_level.setAttribute("autoplay", "autoplay")
+   sound_level.setAttribute("preload", "auto")
+   document.body.appendChild(sound_level)
+}
 
-   FACIL.addEventListener("mouseover", () =>
-   {
-      soundLevel()
-   })
+FACIL.addEventListener("mouseover", () =>
+{
+   soundLevel()
+})
 
-   MEDIO.addEventListener("mouseover", () =>
-   {
-      soundLevel()
-   })
+MEDIO.addEventListener("mouseover", () =>
+{
+   soundLevel()
+})
 
-   SUPREMO.addEventListener("mouseover", () =>
-   {
-      soundLevel()
-   })
+SUPREMO.addEventListener("mouseover", () =>
+{
+   soundLevel()
+})
 
-   FACIL.addEventListener("click", () =>
-   {
-      return n = "facil"
-   })
+FACIL.addEventListener("click", () =>
+{
+   return n = "facil"
+})
 
-   MEDIO.addEventListener("click", () =>
-   {
-      return n = "medio"
-   })
+MEDIO.addEventListener("click", () =>
+{
+   return n = "medio"
+})
 
-   SUPREMO.addEventListener("click", () =>
-   {
-      return n = "supremo"
-   })
-// })
+SUPREMO.addEventListener("click", () =>
+{
+   return n = "supremo"
+})
